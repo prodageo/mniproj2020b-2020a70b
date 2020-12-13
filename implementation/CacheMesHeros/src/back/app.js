38a644e0856314a5885d84477975766ce445a21a
Aurélie <aurelie.beaugeard@insa-rouen.fr>
2020-12-13 21:47:38 +0100

    fixed lint syntax

--- implementation/CacheMesHeros/src/back/app.js
+++ implementation/CacheMesHeros/src/back/app.js
@@ -16,7 +16,7 @@ const httpServer = http.createServer(app)
 const redisPort = 6379
 const redisClient = redis.createClient(redisPort)
 const port = 3000
-let db 
+let db
 let imageObtenue
 
 app.use('/api/cachemesheros', express.static('public'))
@@ -29,83 +29,73 @@ httpServer.listen(port, () => {
   console.log('listening on *:' + port + '\n')
 })
 
-MongoClient.connect(url,  { useUnifiedTopology: true }, (err, client) =>{
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
   if (err) throw err
   db = client.db(dbName)
   let count = 1
   console.log('Connecté à MongoDB ' + db.databaseName + ' database')
 
-  //On regarde si la collection heroImg existe déjà
-  db.listCollections({name:'heroImg'})
+  // On regarde si la collection heroImg existe déjà
+  db.listCollections({ name: 'heroImg' })
     .next((err, collinfo) => {
-
-      //Si ce n'est pas le cas on la crée et la remplit des 5 images du dossier images
+      if (err) throw err
+      // Si ce n'est pas le cas on la crée et la remplit des 5 images du dossier images
       if (!collinfo) {
         db.createCollection('heroImg', (err, result) => {
           if (err) throw err
           console.log('Collection is created ! ')
         })
 
-        readImages('./images/',(filename, content) => {
-          db.collection('heroImg').insertOne({id : count, data : content}, (err,res)=>{
-            if (err) throw err
-            console.log("1 image inserted")
+        readImages('./images/', function (content) {
+          db.collection('heroImg').insertOne({ id: count, data: content }, (err, res) => {
+            if (err) { throw err }
+            console.log('1 image inserted')
           })
-          count ++
-        }), (err)=>{
-          throw err
-        }
-
+          count++
+        })
       }
-  });
-  //client.close() ça je sais pas où le mettre
+    })
+  // client.close() ça je sais pas où le mettre
 })
 
-redisClient.on('error', (err)=>{
+redisClient.on('error', (err) => {
   console.error(err)
 })
 
 const io = new Server(httpServer)
 
 io.on('connect', (client) => {
-
   console.log('Client connecté')
 
   client.on('chercherImg', async () => {
-
     // On choisit un ID au hasard compris entre 1 et 5
     const randomnumber = Math.floor(Math.random() * 5) + 1
 
     // On cherche l'image correspondant à l'ID dans le cache
-    redisClient.get(randomnumber.toString(), async (err,image)=>{
+    redisClient.get(randomnumber.toString(), async (err, image) => {
       if (err) throw err
 
       // Si on trouve l'image on la renvoit et on l'affiche
       if (image) {
-        client.emit('afficherImg', { 
-          img : image 
+        client.emit('afficherImg', {
+          img: image
         })
-      }
-
-      else {
-        //On cherche l'image dans la BD
-        await getImageInDB(randomnumber,db)
-          .then((result)=>{
+      } else {
+        // On cherche l'image dans la BD
+        await getImageInDB(randomnumber, db)
+          .then((result) => {
             imageObtenue = result
           })
-          .catch((error)=>{console.error(error)})
+          .catch((error) => { console.error(error) })
 
         // On l'ajoute au cache
-        redisClient.setnx(randomnumber.toString(),imageObtenue.data)
+        redisClient.setnx(randomnumber.toString(), imageObtenue.data)
 
         // On renvoit l'image au client et on l'affiche
-        client.emit('afficherImg', { 
-          img : imageObtenue.data 
+        client.emit('afficherImg', {
+          img: imageObtenue.data
         })
       }
-    })    
+    })
   })
 })
-
-
-
--- implementation/CacheMesHeros/src/back/utils.js
+++ implementation/CacheMesHeros/src/back/utils.js
@@ -3,13 +3,13 @@ import fs from 'fs'
 export function readImages (dirname, onFileContent) {
   fs.readdir(dirname, (err, filenames) => {
     if (err) {
-      console.error(err);
+      console.error(err)
       return
     }
     filenames.forEach((filename) => {
       fs.readFile(dirname + filename, 'base64', (err, content) => {
         if (err) {
-          console.error(err);
+          console.error(err)
           return
         }
         onFileContent(filename, content)
@@ -18,15 +18,14 @@ export function readImages (dirname, onFileContent) {
   })
 }
 
-export function getImageInDB(randomnumber,db) {
-    return new Promise((resolve,reject)=>{
-        db.collection('heroImg').findOne({id : randomnumber}, (err, result)=>{
-            if (err) {
-                reject(err)
-            }
-            else {
-                resolve(result)
-            }
-        })
+export function getImageInDB (randomnumber, db) {
+  return new Promise((resolve, reject) => {
+    db.collection('heroImg').findOne({ id: randomnumber }, (err, result) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(result)
+      }
     })
+  })
 }
